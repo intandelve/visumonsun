e83ca62aa596bf8c3bec3abdc22719a37de0c068
e31210927_Intan <114232275+intandelve@users.noreply.github.com>
2025-11-29 23:21:30 +0800

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,9 +28,9 @@
                 <div class="py-12">
                     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                         
-                        <div class="flex flex-col md:flex-row gap-6">
+                        <div class="grid grid-cols-1 md:grid-cols-4 gap-6">
             
-                            <div class="w-full md:w-1/4">
+                            <div class="w-full md:col-span-1">
                                 <div class="bg-white p-6 rounded-lg shadow-sm h-fit">
                                     <h3 class="text-lg font-semibold mb-4 text-gray-900">Admin Menu</h3>
                                     <nav class="flex flex-col space-y-2">
@@ -58,7 +58,7 @@
                                 </div>
                             </div>
             
-                            <div class="w-full md:w-3/4">
+                            <div class="w-full md:col-span-3">
                                 {{ $slot }}
                             </div>
             
